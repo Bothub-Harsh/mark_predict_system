0ae8b409a97618004be7353eee6b76d9173a28cd
Harsh Meshram <harshmeshram838@gmail.com>
2026-02-27 00:35:27 +0530

    add

--- project-1/ai_module/predict.py
+++ project-1/ai_module/predict.py
@@ -1,8 +1,13 @@
 import pickle
 import numpy as np
+import os
 import streamlit as st 
 
-load_model=pickle.load(open("../models/model.pkl","rb"))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+model_path = os.path.join(BASE_DIR, "models", "model.pkl")
+
+with open(model_path, "rb") as f:
+    load_model = pickle.load(f)
 
 st.title("📊 Student Marks Predictor")
 st.write("Enter study hours to predict marks")
